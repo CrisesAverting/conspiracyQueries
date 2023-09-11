50445b198cc84ae55a8e3ce129433f3f487504c2
Peter McKasty <pmck25@yahoo.com>
2023-09-11 19:41:57 -0400

    added package importing for the checkoutform model and middleware for stripe

---
+++ server/models/checkoutForm.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('insert key when I sign up');
