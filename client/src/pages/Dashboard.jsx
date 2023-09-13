967aaaddae72b5533396cebce06e05d86a170c57
nicco <mannythemanmoth@gmail.com>
2023-09-12 20:57:42 -0400

    added login page, signup an shop pages

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
 import '../../style.css';
 // import { Link } from "react-router-dom";
-export default function Shop() {
+export default function Dashboard() {
     return (
         <>
             <p>
-                Welcome to Conspiracy queries Shop
+                Welcome to your dashboard "insert username here"
             </p>
         </>
 
