dfa208910fee231316903e6ebea8d6b0c2f5ca39
konawave <nickkjeffs@gmail.com>
2023-09-17 21:50:29 -0400

    pushing seeders to main

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -74,45 +74,6 @@ const resolvers = {
       return { token, profile };
     },
 
-
-    
-    // Define your mutation resolvers here
-    // Example: createUser, createCategory, createProduct, createCartItem
-    //   getUserById: async (_, { userId }) => {
-    //     try {
-    //       return await User.findById(userId);
-    //     } catch (error) {
-    //       throw new Error('Error fetching user by ID');
-    //     }
-    //   },
-
-    //   // Resolver for getting all categories
-    //   getAllCategories: async () => {
-    //     try {
-    //       return await Categories.find();
-    //     } catch (error) {
-    //       throw new Error('Error fetching categories');
-    //     }
-    //   },
-
-    //   // Resolver for getting all products
-    //   getAllProducts: async () => {
-    //     try {
-    //       return await Products.find().populate('Categories');
-    //     } catch (error) {
-    //       throw new Error('Error fetching products');
-    //     }
-    //   },
-
-      // Resolver for getting all cart items
-      // getAllCartItems: async () => {
-      //   try {
-      //     return await Cart.find();
-      //   } catch (error) {
-      //     throw new Error('Error fetching cart items');
-    // }
-
-
 },
 }
 module.exports = resolvers;
