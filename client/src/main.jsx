9210f323e576f6ee65586ed0a44aa83efc183c59
nicco <mannythemanmoth@gmail.com>
2023-09-18 23:36:50 -0400

    order matters

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom/client';
 import React from 'react'
+import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 
