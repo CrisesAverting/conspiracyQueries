361f43e4c1f5de5a02ce6869f71b277f577743eb
nicco <mannythemanmoth@gmail.com>
2023-09-18 23:10:23 -0400

    oops

---
+++ client/src/main 2.jsx
@@ -0,0 +1,45 @@
+import ReactDOM from 'react-dom/client';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+// import './styles.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/signup';
+import Dashboard from './pages/Dashboard';
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      }, 
+      {
+        path: '/login',
+        element: <Login />
+      },
+       {
+        path: '/signup',
+        element: <Signup />
+      },
+      {
+        path: '/Dashboard',
+        element: <Dashboard />
+      }
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
---
+++ client/src/utils/mutations 2.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      profile {
+        userId
+        username
+        password
+        email
+      }
+    }
+  }
+`;
+
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        userId
+        username
+      }
+    }
+  }
+`;
