3efc7bb4eab112aae9c8998eed0fb6fbd1483efe
nicco <mannythemanmoth@gmail.com>
2023-09-18 20:48:21 -0400

    auth for other pages

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 // import '../../style.css';
+import Auth from "../utils/auth";
 // import { Link } from "react-router-dom";
 export default function Dashboard() {
+
     return (
         <>
             <p>
