6535e0dee85c2612872b912e604e7b8068d8a1bc
konawave <nickkjeffs@gmail.com>
2023-09-19 18:19:39 -0400

    still working on fixing rendering. Getting 404 error when launching

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,9 @@
-const {User, Categories, Products, Cart  } = require('../models'); // Import your Mongoose models
-
-const {signToken, AuthenticationError} = require ('../utils/auth');
-
-const findUserByEmail = async (email) => {
-console.log(email)
-  try {
-    return await User.findOne({ email });
-  } catch (error) {
-      throw new Error('Error finding user by email');
-  }
-};
+const { User } = require('../models'); // Import your Mongoose models
+const { AuthenticationError } = require('../utils/auth');
 
 const createUser = async (username, email, password) => {
   try {
     const profile = await User.create({ username, email, password });
-   console.log(profile)
     const token = signToken(profile);
     return { token, profile };
   } catch (error) {
@@ -24,56 +13,48 @@ const createUser = async (username, email, password) => {
 
 const resolvers = {
   Query: {
-    // Resolver for getting a user by ID
-    // 
     profiles: async () => {
       return User.find();
     },
-   
+
     profile: async (_, { profileId }) => {
       return User.findOne({ userId: profileId });
     },
-      me: async (parent, args, context) => {
-        if (context.user) {
-          return User.findOne({ userId: context.user.userId });
-        }
-        throw AuthenticationError;
-      },
+
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ userId: context.user.userId });
+      }
+      throw new AuthenticationError('User not authenticated');
+    },
   },
-  
-  
-  
-      
 
   Mutation: {
     createUser: async (_, { username, email, password }) => {
-      try{
-      // const profile = await User.create({ username, email, password });
-      // const token = signToken(profile);
+      try {
+        return await createUser(username, email, password);
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
 
-      return await createUser(username, email, password);
-      // { token, profile };
-    } catch(error) {
-    //  console.log(profile)
-      // throw new Error(error.message);
-    }
-  },
     login: async (_, { email, password }) => {
       const profile = await User.findOne({ email });
 
       if (!profile) {
-        throw AuthenticationError;
+        throw new AuthenticationError('User not found');
       }
+
       const correctPw = await profile.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw AuthenticationError;
+        throw new AuthenticationError('Incorrect password');
       }
 
       const token = signToken(profile);
       return { token, profile };
     },
+  },
+};
 
-},
-}
 module.exports = resolvers;
