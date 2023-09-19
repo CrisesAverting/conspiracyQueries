3bf84a901138e6c6edcfc160acddc5e74dcae9b9
konawave <nickkjeffs@gmail.com>
2023-09-19 14:17:35 -0400

    ugh almost there

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cq_DB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/cq_DB');
 
 module.exports = mongoose.connection;
